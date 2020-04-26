c21835a14772b0c53a918015141cb625a46763dc
Kevin Beal <tbeal@umd.edu>
2020-04-25 21:52:34 -0400

    Updates

---
+++ precache-manifest.263829ab7867030a639d887ce6e0e1b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1add1691b300b23f03da590b3b98d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6172f984c4f1dfa1e17",
+    "url": "/static/css/main.dd885281.chunk.css"
+  },
+  {
+    "revision": "280802140bce5b556011",
+    "url": "/static/js/2.e368fee0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e368fee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6172f984c4f1dfa1e17",
+    "url": "/static/js/main.d28ca78e.chunk.js"
+  },
+  {
+    "revision": "147882dbe35565353fd6",
+    "url": "/static/js/runtime-main.2989194e.js"
+  },
+  {
+    "revision": "4c9c1f1eacb853bf7062a2ac246d7d19",
+    "url": "/static/media/A Goblin Appears.4c9c1f1e.otf"
+  },
+  {
+    "revision": "ab3faa69a9039dae9fc86b4905081113",
+    "url": "/static/media/boilwater.ab3faa69.png"
+  },
+  {
+    "revision": "76ce43e2a71bddaa8d8c59addb826727",
+    "url": "/static/media/vegan_icon.76ce43e2.png"
+  }
+]);
