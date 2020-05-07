56e82dc582bb8c087991b03b71fa0bb0e9bda461
Kevin Beal <tbeal@umd.edu>
2020-05-06 20:12:19 -0400

    updated

---
+++ build/precache-manifest.ddcb81162540ffb80e81ea29f5eea191.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abae0339e324e78cff94059605def503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b70f84cb5cfb053e80",
+    "url": "/static/css/main.d94c0efe.chunk.css"
+  },
+  {
+    "revision": "dbd21fd9a712cbb634d5",
+    "url": "/static/js/2.fb949060.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb949060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b70f84cb5cfb053e80",
+    "url": "/static/js/main.bcfd7902.chunk.js"
+  },
+  {
+    "revision": "147882dbe35565353fd6",
+    "url": "/static/js/runtime-main.2989194e.js"
+  },
+  {
+    "revision": "4c9c1f1eacb853bf7062a2ac246d7d19",
+    "url": "/static/media/A Goblin Appears.4c9c1f1e.otf"
+  },
+  {
+    "revision": "1282b17b0514b3ca7d25883254e3813b",
+    "url": "/static/media/Loading.1282b17b.gif"
+  },
+  {
+    "revision": "ab3faa69a9039dae9fc86b4905081113",
+    "url": "/static/media/boilwater.ab3faa69.png"
+  },
+  {
+    "revision": "4de847c1eb425c9fe568588019bf7c2b",
+    "url": "/static/media/choppedgarlic.4de847c1.png"
+  },
+  {
+    "revision": "7bd04def9d896d0a41b0aacd7d1ad926",
+    "url": "/static/media/choppedonions.7bd04def.png"
+  },
+  {
+    "revision": "d96daa55a7cf44bb6aec827dae56b6ad",
+    "url": "/static/media/cookedrice.d96daa55.png"
+  },
+  {
+    "revision": "814d8e0cffd8f1fa97c500deeaa5c5aa",
+    "url": "/static/media/cutveggies.814d8e0c.png"
+  },
+  {
+    "revision": "718d0429b13d0874e645919f7bc13d0b",
+    "url": "/static/media/glutenfree.718d0429.png"
+  },
+  {
+    "revision": "289f61899c40c37dd5473d873f499ff3",
+    "url": "/static/media/halal.289f6189.png"
+  },
+  {
+    "revision": "234612fdd72e668560b77e0007b0f76c",
+    "url": "/static/media/kosher.234612fd.png"
+  },
+  {
+    "revision": "2ab0c31fc25d7d52acc02c1cb74837b6",
+    "url": "/static/media/lowfat.2ab0c31f.png"
+  },
+  {
+    "revision": "13d77c5c26c15bbe738c05cc1ccafeff",
+    "url": "/static/media/nutfree.13d77c5c.png"
+  },
+  {
+    "revision": "997c546ec332df5fcb81224776b76f7e",
+    "url": "/static/media/organic.997c546e.png"
+  },
+  {
+    "revision": "d9659fbfe58803cd2ab03246b98cbc08",
+    "url": "/static/media/silhouette.d9659fbf.png"
+  },
+  {
+    "revision": "3e5d8a24c41f3bb5a0b296acbdc67a0c",
+    "url": "/static/media/spicy.3e5d8a24.png"
+  },
+  {
+    "revision": "77772c118817b8f0a0456de60e130aaf",
+    "url": "/static/media/stirfriedrice.77772c11.png"
+  },
+  {
+    "revision": "76ce43e2a71bddaa8d8c59addb826727",
+    "url": "/static/media/vegan.76ce43e2.png"
+  }
+]);
