57db8c813e4b7e884bf8e846d0415099f17c5aea
Kevin Beal <timothykevin.beal@gmail.com>
2021-04-24 13:59:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3f9bda549cf94094d52c835221e698b.js"
+  "/precache-manifest.270f9a662d932599906ef6ecd0c8b857.js"
 );
 
 self.addEventListener('message', (event) => {
